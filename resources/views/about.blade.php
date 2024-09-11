bce71ef64c79b1b88ee7231225f6b5d589627206
adilelka06 <adilelka06@hotmail.com>
2024-09-11 15:14:11 +0200

    laatse dingen

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.app title="Over Ons">
     <h1>Over ons</h1>
     <p> Wij zijn een jong en dynamisch team dat zich richt op het ontwikkelen van innovatieve weboplossingen. Ons doel is om gebruikersvriendelijke en efficiënte websites te bouwen voor bedrijven van elke omvang. Onze passie ligt in het verbeteren van de digitale ervaringen van onze klanten. </p>
+    <img src="{{ asset('img/smart_home.jpeg') }}" alt="smart home">
 </x-layouts.app>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.app title="Welkom op onze website!">
     <h1>welkom op de homepage van Smart Solutions</h1>
     <p>We zijn verheugd dat je hier bent. Op deze site vind je meer informatie over wie we zijn en hoe je contact met ons kunt opnemen. Gebruik de navigatie om naar de andere pagina's te gaan. </p>
+    <img src="{{ asset('img/welcome.jpg') }}" alt="welcome">
 </x-layouts.app>
