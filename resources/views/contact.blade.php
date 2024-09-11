8d98e456fb19d61f59ca1e8bf1b0fe4804a23c7b
adilelka06 <adilelka06@hotmail.com>
2024-09-11 14:59:37 +0200

    form aangepast

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,15 +2,19 @@
     <h1>Contact</h1>
     <p>Heb je vragen of opmerkingen? Neem dan gerust contact met ons op. We helpen je graag verder. </p>
     <form action="#" method="post">
-        <div>
+        <div class="form-group">
             <label for="name">Naam</label>
             <input type="text" id="name" name="name">
         </div>
-        <div>
+        <div class="form-group">
             <label for="email">E-mail</label>
             <input type="email" id="email" name="email">
         </div>
-        <div>
+        <div class="form-group">
+            <label for="subject">Onderwerp</label>
+            <input type="text" id="subject" name="subject">
+        </div>
+        <div class="form-group">
             <label for="message">Bericht</label>
             <textarea id="message" name="message"></textarea>
         </div>
